ece922a9537f5efac0d921410aed14567c91c171
Raymond Spencer <rts@udel.edu>
2025-03-19 22:54:26 -0400

    Submitted local changes

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
- add-name-ysuraibi
+ add-name-ysuraibi - add-name-squidbool
           Chiristian Palmer, Yazan Alsuraibi, Ray Spencer.
 
         </p>
