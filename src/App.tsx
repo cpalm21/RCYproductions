5b864b0157dc18864cbae6e0faea6616bc54acb1
ysuraibi <ysuraibi@udel.edu>
2025-03-31 15:57:27 -0400

    Simple change

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
 
-          Chiristian Palmer, Yazan Alsuraibi, Ray Spencer...
+          Chiristian Palmer, Yazan Alsuraibi, Ray Spencer. Last one
 
         </p>
         <a
