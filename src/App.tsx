7c13ead568381456294becacb0ced3a6b3af31e2
Christian <palmerc@udel.edu>
2025-04-01 13:46:34 -0400

    Added a page for the Detailed Quiz and added buttons/routes to go from home page to detailed quiz page and back

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import React, { useState } from 'react';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 import { Homepage } from "./Homepage";
+import {DetailedQuizPage} from "./DetailedQuizPage"
+import {Routes, Route} from "react-router-dom"
+
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -27,6 +30,13 @@ function App() {
   }
   return (
     <div className="App">
+      <Routes>
+      <Route path = "/DetailedQuizPage" element={<DetailedQuizPage/>}/>
+      <Route 
+      path="/"
+      element={
+        <div>
+    
       <header className="App-header">
       <h1 className="App-title">Career Assessment Quiz</h1>
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
@@ -44,14 +54,25 @@ function App() {
         </a>
         */}
       </header>
+      
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
-    </div>
+        </div>
+
+       }
+       />
+       </Routes>
+       </div>
+
+
+
+
   );
 }
+
 
 export default App;
--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,15 +1,29 @@
 import { Button } from "react-bootstrap";
+import DetailedQuizPage from "./DetailedQuizPage"
+import {Routes, Route} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
+
 
 export function Homepage(): React.JSX.Element {
+  
+  const navigate = useNavigate();
+
+  const goToDetailed = () => {
+    navigate('/DetailedQuizPage')
+  }
+
+
+  return (
+
+
    
-    return (
 <span>
-  <Button size="lg" style={{ marginRight: "400px", padding: "120px 24px" }}>
-    Detailed Quiz
-  </Button>
-  <Button size="lg" style={{ padding: "120px 45px" }}>
-    Basic Quiz
-  </Button>
+<Button onClick={goToDetailed} size="lg" style={{ marginRight: "400px", padding: "120px 24px" }}>
+  Detailed Quiz
+</Button>
+<Button size="lg" style={{ padding: "120px 45px" }}>
+  Basic Quiz
+</Button>
 </span>
-    );
+  );
 }
